cfeeccd9b51095ce1787c85c9a33144d4d29005b
Jack Lloyd <jack@randombit.net>
2017-04-04 16:01:18 -0400

    Attempt to cut down Travis build times

--- src/scripts/ci/travis/main.sh
+++ src/scripts/ci/travis/main.sh
@@ -4,6 +4,10 @@ which shellcheck > /dev/null && shellcheck "$0" # Run shellcheck on this if avai
 
 PARENT_DIR=$(dirname "$0")
 
+if [ "$TRAVIS_EVENT_TYPE" == "pull_request" ] && [ -n "$EXPENSIVE_TEST" ]; then
+    (git log -1 "$TRAVIS_COMMIT_RANGE" | grep '\[run expensive tests\]' > /dev/null) || exit 0;
+fi
+
 if [ "$BUILD_MODE" = "lint" ]; then
     "$PARENT_DIR"/lint.sh
 else
